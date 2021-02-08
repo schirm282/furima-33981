c70fb754e7a4a24ccf0ad5cd188a1eebefa102c4
schirm282 <77378163+schirm282@users.noreply.github.com>
2021-02-08 18:08:45 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'adminK' && password == '0282j'
+    end
+  end
 end

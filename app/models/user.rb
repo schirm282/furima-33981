021cc7a315b46736995ae479115ce35c71cd44ee
schirm282 <77378163+schirm282@users.noreply.github.com>
2021-02-12 17:00:35 +0900

    Userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
  
   validates :nickname, presence: true
+  validates :email, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_frigana, presence: true
